386bd718d1dfb62a553b90f6c13ddb7b34bcf84a
Kate Wood <katewood611@gmail.com>
2017-05-08 23:47:20 -0700

    adds middleware and db configurations

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
-var express = require('express');
+var express = require('express'),
+  mongoose = require('mongoose'),
+  mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/react-cats',
+  logger = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  path = require('path')
+
+mongoose.connect(mongoUri)
 
 // Create our app
-var app = express();
-const PORT = process.env.PORT || 3000;
+var app = express()
+const PORT = process.env.PORT || 3000
 var apiRoutes = require('./app/api/config/api_routes')
 
 app.use(function(req, res, next){
   if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
+    res.redirect('http://' + req.hostname + req.url)
   } else {
-    next();
+    next()
   }
-});
+})
 
-app.use(express.static('public'));
+app.use(express.static('public'))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 app.use('/api', apiRoutes)
 
 app.listen(PORT, function() {
-  console.log('Express server is up on port ' + PORT);
-});
+  console.log('Express server is up on port ' + PORT)
+})
